c899d7343ee96583f759573561c0e684cf87554c
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-03-26 14:15:22 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/app_diario/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_diario
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+//import 'screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Diário',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        //'/': (context) => LoginScreen(),
+        //'/home': (context) => HomeScreen(),
+        //'/addPlan': (context) => AddPlanScreen(),
+      },
+    );
+  }
+}
